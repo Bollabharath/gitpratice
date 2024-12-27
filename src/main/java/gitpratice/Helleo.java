f373659fcf0d3e98fcb893bc3e7c4e11a58b166b
bharath <bharathbolla013@gmail.com>
2024-12-27 22:15:22 +0530

    second commit

---
+++ src/main/java/gitpratice/Helleo.java
@@ -0,0 +1,9 @@
+package gitpratice;
+
+public class Helleo {
+
+	public void gitSecond_commit() {
+		System.out.println("Heloworld");
+		
+	}
+}
---
+++ target/classes/Telugu.java
@@ -0,0 +1,8 @@
+
+public class Telugu {
+	
+	public main() {
+		System.out.println("This is the change inthe commit");
+	}
+
+}
