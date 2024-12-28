0ef0d055668b5b0ea8b01f249b32ac51b6bf86a6
bharath <bharathbolla013@gmail.com>
2024-12-28 11:53:47 +0530

    again

---
+++ src/main/java/gitpratice/commit/push/Commit.java
@@ -0,0 +1,16 @@
+package gitpratice.commit.push;
+
+public class Commit {
+	
+	private void Syso() {
+		// TODO Auto-generated method stub
+
+		System.out.println("this is commit");
+		System.out.println("this is commit");
+		System.out.println("this is commit");
+		System.out.println("this is commit");
+		System.out.println("this is commit");
+		System.out.println("this is commit");
+	}
+
+}
